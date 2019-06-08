1043bac8648b02d2eec7b9cd61950cc0a6147c78
Andrew Davisson <akdavisson4@gmail.com>
2019-06-08 20:29:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
     pos_3 = board[win_index_3]
     
     if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") || (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
-      win_combo
+      return win_combo
     end
   end
 end
