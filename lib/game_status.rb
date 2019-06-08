18ae515c4dc0aa8f0215ca085f5f36db8d61638d
Andrew Davisson <akdavisson4@gmail.com>
2019-06-08 19:53:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,14 +15,14 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def won?(board)
-  WIN_COMBINATIONS.any? do |win_combo|
-    if((win_combo.all?{|idx| position_taken?(board,idx)} &&
-      board[win_combo[0]] == "X" && board[win_combo[1]] == "X" &&
-      board[win_combo[0]] == "X") || (win_combo.all?{|idx| position_taken?(board,idx)} &&
-      board[win_combo[0]] == "O" && board[win_combo[1]] == "O" &&
-      board[win_combo[0]] == "O"))
-      win_combo
-    end
-  end
-end
+#def won?(board)
+#  WIN_COMBINATIONS.any? do |win_combo|
+#    if((win_combo.all?{|idx| position_taken?(board,idx)} &&
+#      board[win_combo[0]] == "X" && board[win_combo[1]] == "X" &&
+#      board[win_combo[0]] == "X") || (win_combo.all?{|idx| position_taken?(board,idx)} &&
+#      board[win_combo[0]] == "O" && board[win_combo[1]] == "O" &&
+#      board[win_combo[0]] == "O"))
+#      win_combo
+#    end
+#  end
+#end
